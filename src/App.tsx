76daae3a82695d3ba03f6f77099dff7eb57baa9f
Naimur Reza <naimur.rezaa@gmail.com>
2024-01-10 23:53:31 +0600

    Add box task

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,12 @@ import { useAppDispatch, useAppSelector } from "./redux/hook";
 
 const App = () => {
   const count = useAppSelector((state) => state.counter.value);
+  const boxCount = useAppSelector((state) => state.counter.boxCount);
   const dispatch = useAppDispatch();
 
+  console.log(boxCount);
   return (
-    <div className=" flex items-center justify-center min-h-screen">
+    <div className=" flex items-center justify-center min-h-screen flex-col">
       <div className="border bg-gray-100 w-80 flex justify-center items-center gap-10 h-32 rounded mx-10 my-10">
         <button
           className="bg-teal-500 text-white  px-5 py-1 rounded-md"
@@ -29,6 +31,12 @@ const App = () => {
           onClick={() => dispatch(incrementByAmount(5))}>
           +5
         </button>
+      </div>
+
+      <div className="flex gap-2">
+        {Array.from({ length: boxCount }).map(() => (
+          <p className="h-5 w-5 border border-pink-500"></p>
+        ))}
       </div>
     </div>
   );
--- src/redux/features/counterSlice.ts
+++ src/redux/features/counterSlice.ts
@@ -2,10 +2,12 @@ import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
 interface counterState {
   value: number;
+  boxCount: number;
 }
 
 const initialState: counterState = {
   value: 0,
+  boxCount: 0,
 };
 
 export const counterSlice = createSlice({
@@ -14,12 +16,16 @@ export const counterSlice = createSlice({
   reducers: {
     increment: (state) => {
       state.value += 1;
+      const shouldAddBox = state.value % 10 == 0;
+      state.boxCount = shouldAddBox ? state.boxCount + 1 : state.boxCount;
     },
     decrement: (state) => {
       state.value -= 1;
     },
     incrementByAmount: (state, action: PayloadAction<number>) => {
       state.value += action.payload;
+      const shouldAddBox = state.value % 10 == 0;
+      state.boxCount = shouldAddBox ? state.boxCount + 1 : state.boxCount;
     },
   },
 });
